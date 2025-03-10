3e3bc2005e157dcd598f3dcb0f5ef42dabbf305d
박도연 <121646938+gitdoyeon@users.noreply.github.com>
2025-03-10 15:47:46 +0900

    print문 (디버그 실습)

--- src/FirstJava.java
+++ src/FirstJava.java
@@ -1,5 +1,16 @@
 public class FirstJava {
     public static void main(String[] args) {
-        System.out.println("parkdoyeon");
+        System.out.println("Hello World!");
+
+        System.out.println("Hello Seoul!");
+
+        System.out.println("Hello Busan!");
+
+        System.out.println("Hello jeju!");
+
+        System.out.println("Hello Beawha!");
+
     }
 }
+
+// debug : 코딩 과정을 볼 수 있는 것 (run와 차이)
